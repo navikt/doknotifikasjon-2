eb205b0cbde6ab481e1bc55c9625cf2e88c33824
Heidi Elisabeth <heidi.elisabeth.sando@nav.no>
2020-09-23 09:14:13 +0200

    MMA-4712: Opprette KNOT001 - Start på integrasjon mot sts

---
+++ core/src/main/java/no/nav/doknotifikasjon/exception/technical/StsTechnicalException.java
@@ -0,0 +1,7 @@
+package no.nav.doknotifikasjon.exception.technical;
+
+public class StsTechnicalException extends AbstractDoknotifikasjonTechnicalException{
+    public StsTechnicalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
