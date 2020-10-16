d5e6d908e2a0d2a688e8ed32acf92238bfb469a0
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2020-10-16 14:53:57 +0200

    MMA-4715: Etablere Knot004 - Fjerne kode som er hentet fra knot001-branch for hjelp til testoppsett

---
+++ knot001/src/main/java/no/nav/doknotifikasjon/domain/Doknotifikasjon.java
@@ -0,0 +1,16 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Doknotifikasjon {
+	public String bestillingsId;
+	public String bestillerId;
+	public String fodselsnummer;
+	public Integer antallRenotifikasjoner;
+	public Integer renotifikasjonIntervall;
+	public String tittel;
+	public String tekst;
+}
