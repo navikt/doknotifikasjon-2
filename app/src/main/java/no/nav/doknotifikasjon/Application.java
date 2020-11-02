cc737c2698058ddf6af58a7d4e2534e138f9c611
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-11-02 16:47:32 +0100

    Rettet feil i byggoppsett.

--- app/src/main/java/no/nav/doknotifikasjon/Application.java
+++ app/src/main/java/no/nav/doknotifikasjon/Application.java
@@ -1,13 +1,11 @@
 package no.nav.doknotifikasjon;
 
-import no.nav.doknotifikasjon.knot002.Knot002Config;
-import no.nav.doknotifikasjon.knot003.Knot003Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import(value = {CoreConfig.class, Knot001Config.class, Knot002Config.class, Knot003Config.class})
+@Import(value = {CoreConfig.class})
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
