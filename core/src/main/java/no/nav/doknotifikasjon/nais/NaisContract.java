6c0d895aad5d485917750068872307fa7f28f806
TimAa-nav-dev <59047094+TimAa-nav-dev@users.noreply.github.com>
2020-11-02 10:10:22 +0100

    Legger til test endepunkt i rest controllen (#64)

--- core/src/main/java/no/nav/doknotifikasjon/nais/NaisContract.java
+++ core/src/main/java/no/nav/doknotifikasjon/nais/NaisContract.java
@@ -3,6 +3,9 @@ package no.nav.doknotifikasjon.nais;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.doknotifikasjon.kafka.KafkaEventProducer;
+import no.nav.doknotifikasjon.schemas.Doknotifikasjon;
+import no.nav.doknotifikasjon.schemas.PrefererteKanal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static no.nav.doknotifikasjon.kafka.KafkaTopics.KAFKA_TOPIC_DOK_NOTIFKASJON;
 
 @Slf4j
 @RestController
@@ -20,11 +27,13 @@ public class NaisContract {
 	private static final String APPLICATION_ALIVE = "Application is alive!";
 	private static final String APPLICATION_READY = "Application is ready for traffic!";
 
+	private final KafkaEventProducer publisher;// todo remove
 
 	private AtomicInteger appStatus = new AtomicInteger(1);
 
 	@Inject
-	public NaisContract(MeterRegistry registry) {
+	public NaisContract(MeterRegistry registry, KafkaEventProducer publisher) {
+		this.publisher = publisher;// todo remove
 		Gauge.builder("dok_app_is_ready", appStatus, AtomicInteger::get).register(registry);
 	}
 
@@ -38,5 +47,28 @@ public class NaisContract {
 		appStatus.set(1);
 
 		return new ResponseEntity<>(APPLICATION_READY, HttpStatus.OK);
+	}
+
+	//TODO remove after testing
+	@GetMapping("/isAlive2")
+	public void kafkaProduceMessage() {
+		List<PrefererteKanal> preferteKanaler = List.of(PrefererteKanal.EPOST, PrefererteKanal.SMS);
+
+		Doknotifikasjon dokEksternNotifikasjon = new Doknotifikasjon(
+				LocalDateTime.now().toString(),
+				LocalDateTime.now().toString(),
+				"08048422250", // FNR er fra en testbrukker hos dolly
+				0,
+				0,
+				"tittel",
+				"epostTekst",
+				"smsTekst",
+				preferteKanaler
+		);
+
+		publisher.publish(
+				KAFKA_TOPIC_DOK_NOTIFKASJON,
+				dokEksternNotifikasjon
+		);
 	}
 }
