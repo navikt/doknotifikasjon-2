f2d5b004b020013900915db403dca3ef88956701
erlendaxelsson <erlend.axelsson@gmail.com>
2020-11-05 11:31:24 +0100

    Add some logging for debug

--- core/src/main/java/no/nav/doknotifikasjon/consumer/sts/STSClientWSTrust13and14.java
+++ core/src/main/java/no/nav/doknotifikasjon/consumer/sts/STSClientWSTrust13and14.java
@@ -1,9 +1,11 @@
 package no.nav.doknotifikasjon.consumer.sts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.Bus;
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.trust.STSClient;
 
+@Slf4j
 public class STSClientWSTrust13and14 extends STSClient {
 
     public STSClientWSTrust13and14(Bus b) {
@@ -24,6 +26,7 @@ public class STSClientWSTrust13and14 extends STSClient {
     @Override
     public SecurityToken requestSecurityToken(String appliesTo, String action, String requestType,
                                               String binaryExchange) throws Exception {
+        log.info("Requesting securityToken args=[{}, {}, {}]", appliesTo, action, requestType);
         return super.requestSecurityToken(appliesTo, action, requestType, binaryExchange);
     }
 }
