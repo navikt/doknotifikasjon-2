cec96262301f335344bef5a2a7dcd116acf2cd87
A157857 <tim.aasen@nav.no>
2021-07-05 21:32:46 +0200

    Pr;ve noe nytt

--- core/src/main/java/no/nav/doknotifikasjon/kafka/KafkaTopics.java
+++ core/src/main/java/no/nav/doknotifikasjon/kafka/KafkaTopics.java
@@ -3,7 +3,7 @@ package no.nav.doknotifikasjon.kafka;
 public class KafkaTopics {
 	public static final String KAFKA_TOPIC_DOK_NOTIFKASJON = "teamdokumenthandtering.privat-dok-notifikasjon";
 	public static final String PRIVAT_DOK_NOTIFIKASJON_MED_KONTAKT_INFO = "teamdokumenthandtering.privat-dok-notifikasjon-med-kontakt-info";
-	public static final String KAFKA_TOPIC_DOK_NOTIFKASJON_STATUS = "teamdokumenthandtering.privat-dok-notifikasjon-status";
+	public static final String KAFKA_TOPIC_DOK_NOTIFKASJON_STATUS = "teamdokumenthandtering.aapen-dok-notifikasjon-status";
 	public static final String KAFKA_TOPIC_DOK_NOTIFKASJON_SMS = "teamdokumenthandtering.privat-dok-notifikasjon-sms";
 	public static final String KAFKA_TOPIC_DOK_NOTIFKASJON_EPOST = "teamdokumenthandtering.privat-dok-notifikasjon-epost";
 	public static final String KAFKA_TOPIC_DOK_NOTIFIKASJON_STOPP = "teamdokumenthandtering.privat-dok-notifikasjon-stopp";
