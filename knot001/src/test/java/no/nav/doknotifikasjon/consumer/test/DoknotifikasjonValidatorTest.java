41e2767507c0fd03fd1d00dec6cef07d018592ad
Christian Tumyr <christian.tumyr@nav.no>
2022-06-20 12:54:01 +0200

    La til tester for validering av renotifikasjoner
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- knot001/src/test/java/no/nav/doknotifikasjon/consumer/test/DoknotifikasjonValidatorTest.java
+++ knot001/src/test/java/no/nav/doknotifikasjon/consumer/test/DoknotifikasjonValidatorTest.java
@@ -5,6 +5,9 @@ import no.nav.doknotifikasjon.exception.functional.InvalidAvroSchemaFieldExcepti
 import no.nav.doknotifikasjon.kafka.KafkaStatusEventProducer;
 import no.nav.doknotifikasjon.schemas.Doknotifikasjon;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,7 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createDoknotifikasjon;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createDoknotifikasjonWithInvalidAntallRenotifikasjoner;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createDoknotifikasjonWithInvalidFnr;
-import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.RENOTIFIKASJON_INTERVALL_REQUIRES_ANTALL_RENOTIFIKASJONER;
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.ANTALL_RENOTIFIKASJONER_REQUIRES_RENOTIFIKASJON_INTERVALL;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
@@ -36,7 +39,7 @@ class DoknotifikasjonValidatorTest {
 		assertThrows(InvalidAvroSchemaFieldException.class, () -> doknotifikasjonValidator.validate(doknotifikasjon));
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
-				doknotifikasjon.getBestillingsId(), doknotifikasjon.getBestillerId(), RENOTIFIKASJON_INTERVALL_REQUIRES_ANTALL_RENOTIFIKASJONER, null
+				doknotifikasjon.getBestillingsId(), doknotifikasjon.getBestillerId(), ANTALL_RENOTIFIKASJONER_REQUIRES_RENOTIFIKASJON_INTERVALL, null
 		);
 	}
 
@@ -107,5 +110,32 @@ class DoknotifikasjonValidatorTest {
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
 				doknotifikasjon.getBestillingsId(), doknotifikasjon.getBestillerId(), "string kan ikke være større enn 30", null
 		);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(ints = {0, -2})
+	void shouldFailValidateOnInvalidRenotifikasjonIntervall(Integer intervall) {
+		Doknotifikasjon doknotifikasjon = createDoknotifikasjon();
+		doknotifikasjon.setAntallRenotifikasjoner(5);
+		doknotifikasjon.setRenotifikasjonIntervall(intervall);
+		assertThrows(InvalidAvroSchemaFieldException.class, () ->
+				doknotifikasjonValidator.validateRenotifikasjoner(doknotifikasjon)
+		);
+
+		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
+				doknotifikasjon.getBestillingsId(), doknotifikasjon.getBestillerId(), "antallRenotifikasjoner krever at renotifikasjonIntervall er satt", null
+		);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(ints = 0)
+	void shouldValidateOnAntallRenotifikasjonerNullOrZero(Integer antallRenotifikasjoner) {
+		Doknotifikasjon doknotifikasjon = createDoknotifikasjon();
+		doknotifikasjon.setAntallRenotifikasjoner(antallRenotifikasjoner);
+		doknotifikasjon.setRenotifikasjonIntervall(5);
+
+		doknotifikasjonValidator.validateRenotifikasjoner(doknotifikasjon);
 	}
 }
--- knot006/src/test/java/no/nav/doknotifikasjon/consumer/test/NotifikasjonValidatorTest.java
+++ knot006/src/test/java/no/nav/doknotifikasjon/consumer/test/NotifikasjonValidatorTest.java
@@ -5,6 +5,9 @@ import no.nav.doknotifikasjon.exception.functional.InvalidAvroSchemaFieldExcepti
 import no.nav.doknotifikasjon.kafka.KafkaStatusEventProducer;
 import no.nav.doknotifikasjon.schemas.NotifikasjonMedkontaktInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,39 +15,39 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createDoknotifikasjonWithInvalidAntallRenotifikasjoner;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createDoknotifikasjonWithoutEpostOrSms;
 import static no.nav.doknotifikasjon.consumer.TestUtils.createNotifikasjon;
-import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.RENOTIFIKASJON_INTERVALL_REQUIRES_ANTALL_RENOTIFIKASJONER;
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.ANTALL_RENOTIFIKASJONER_REQUIRES_RENOTIFIKASJON_INTERVALL;
 import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.MOBILTELEFONNUMMER_OR_EPOSTADESSE_MUST_BE_SET;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = {NotifikasjonValidator.class})
-class DoknotifikasjonValidatorTest {
+class NotifikasjonValidatorTest {
 
 	@Autowired
-	NotifikasjonValidator doknotifikasjonValidator;
+	NotifikasjonValidator notifikasjonValidator;
 
 	@MockBean
 	KafkaStatusEventProducer statusProducer;
 
 	@Test
 	void shouldValidateAvroSchemaWhenSendingValidAvroSchema() {
-		doknotifikasjonValidator.validate(createNotifikasjon());
+		notifikasjonValidator.validate(createNotifikasjon());
 	}
 
 	@Test
 	void shouldFailValidateAvroSchemaWhenSendingInvalidAvroSchema() {
 		NotifikasjonMedkontaktInfo notifikasjon = createDoknotifikasjonWithInvalidAntallRenotifikasjoner();
-		assertThrows(InvalidAvroSchemaFieldException.class, () -> doknotifikasjonValidator.validate(notifikasjon));
+		assertThrows(InvalidAvroSchemaFieldException.class, () -> notifikasjonValidator.validate(notifikasjon));
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
-				notifikasjon.getBestillingsId(), notifikasjon.getBestillerId(), RENOTIFIKASJON_INTERVALL_REQUIRES_ANTALL_RENOTIFIKASJONER, null
+				notifikasjon.getBestillingsId(), notifikasjon.getBestillerId(), ANTALL_RENOTIFIKASJONER_REQUIRES_RENOTIFIKASJON_INTERVALL, null
 		);
 	}
 
 	@Test
 	void shouldFailValidateWhenMissingEpostadresseAndMobiltelefonnummer() {
 		NotifikasjonMedkontaktInfo notifikasjon = createDoknotifikasjonWithoutEpostOrSms();
-		assertThrows(InvalidAvroSchemaFieldException.class, () -> doknotifikasjonValidator.validate(notifikasjon));
+		assertThrows(InvalidAvroSchemaFieldException.class, () -> notifikasjonValidator.validate(notifikasjon));
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
 				notifikasjon.getBestillingsId(), notifikasjon.getBestillerId(), MOBILTELEFONNUMMER_OR_EPOSTADESSE_MUST_BE_SET, null
@@ -55,7 +58,7 @@ class DoknotifikasjonValidatorTest {
 	void shouldFailValidateStringWhenFieldIsNull() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
 		assertThrows(InvalidAvroSchemaFieldException.class, () ->
-				doknotifikasjonValidator.validateString(notifikasjon, null, 4000, "string")
+				notifikasjonValidator.validateString(notifikasjon, null, 4000, "string")
 		);
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
@@ -67,7 +70,7 @@ class DoknotifikasjonValidatorTest {
 	void shouldFailValidateStringWhenFieldIsEmpty() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
 		assertThrows(InvalidAvroSchemaFieldException.class, () ->
-				doknotifikasjonValidator.validateString(notifikasjon, "         ", 4000, "string")
+				notifikasjonValidator.validateString(notifikasjon, "         ", 4000, "string")
 		);
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
@@ -79,7 +82,7 @@ class DoknotifikasjonValidatorTest {
 	void shouldFailValidateStringWhenFieldIsTooLong() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
 		assertThrows(InvalidAvroSchemaFieldException.class, () ->
-				doknotifikasjonValidator.validateString(notifikasjon, "Test", 2, "string")
+				notifikasjonValidator.validateString(notifikasjon, "Test", 2, "string")
 		);
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
@@ -90,13 +93,13 @@ class DoknotifikasjonValidatorTest {
 	@Test
 	void shouldValidateStringWhenParametersIsCorrect() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
-		doknotifikasjonValidator.validateString(notifikasjon, "Test", 10, "string");
+		notifikasjonValidator.validateString(notifikasjon, "Test", 10, "string");
 	}
 
 	@Test
 	void shouldValidateNumberForSnot001WhenParametersIsCorrect() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
-		doknotifikasjonValidator.validateNumberForSnot001(notifikasjon, 30, "String");
+		notifikasjonValidator.validateNumberForSnot001(notifikasjon, 30, "String");
 	}
 
 
@@ -104,11 +107,38 @@ class DoknotifikasjonValidatorTest {
 	void shouldFailValidateNumberSnot001WhenFieldIsToGreaterThen30() {
 		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
 		assertThrows(InvalidAvroSchemaFieldException.class, () ->
-				doknotifikasjonValidator.validateNumberForSnot001(notifikasjon, 100, "string")
+				notifikasjonValidator.validateNumberForSnot001(notifikasjon, 100, "string")
 		);
 
 		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
 				notifikasjon.getBestillingsId(), notifikasjon.getBestillerId(), "string kan ikke være større enn 30", null
 		);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(ints = {0, -2})
+	void shouldFailValidateOnInvalidRenotifikasjonIntervall(Integer intervall) {
+		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
+		notifikasjon.setAntallRenotifikasjoner(5);
+		notifikasjon.setRenotifikasjonIntervall(intervall);
+		assertThrows(InvalidAvroSchemaFieldException.class, () ->
+				notifikasjonValidator.validateRenotifikasjoner(notifikasjon)
+		);
+
+		verify(statusProducer).publishDoknotifikasjonStatusFeilet(
+				notifikasjon.getBestillingsId(), notifikasjon.getBestillerId(), "antallRenotifikasjoner krever at renotifikasjonIntervall er satt", null
+		);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(ints = 0)
+	void shouldValidateOnAntallRenotifikasjonerNullOrZero(Integer antallRenotifikasjoner) {
+		NotifikasjonMedkontaktInfo notifikasjon = createNotifikasjon();
+		notifikasjon.setAntallRenotifikasjoner(antallRenotifikasjoner);
+		notifikasjon.setRenotifikasjonIntervall(5);
+
+		notifikasjonValidator.validateRenotifikasjoner(notifikasjon);
 	}
 }
