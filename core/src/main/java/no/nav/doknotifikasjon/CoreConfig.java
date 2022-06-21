01ed653c026128b870555c73aa85557260b804aa
Christian Tumyr <christian.tumyr@nav.no>
2022-06-21 14:44:36 +0200

    MMA-6127: Konfigurer Azure for app

--- core/src/main/java/no/nav/doknotifikasjon/CoreConfig.java
+++ core/src/main/java/no/nav/doknotifikasjon/CoreConfig.java
@@ -1,5 +1,6 @@
 package no.nav.doknotifikasjon;
 
+import no.nav.doknotifikasjon.config.AzureConfig;
 import no.nav.doknotifikasjon.config.ServiceuserAlias;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +9,7 @@ import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @ComponentScan
-@EnableConfigurationProperties({ServiceuserAlias.class})
+@EnableConfigurationProperties({ServiceuserAlias.class, AzureConfig.class})
 @EnableRetry
 public class CoreConfig {
 
