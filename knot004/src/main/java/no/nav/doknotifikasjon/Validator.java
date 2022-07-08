2ac410978558b30e7ddb4ab0afc9597ce3b72ba5
Anders Opskar Voldsund <anders.voldsund@nav.no>
2022-07-08 15:04:42 +0200

    MMA-6251 Unngå henting av notifikasjon frå db viss status er feila med spesifikke feilmeldingar i knot004
    I desse situasjonane blir notifikasjonen uansett ikkje oppretta, så det vil ikkje eksistere noko i db.
    
    Co-authored-by: Christian Tumyr <christian.tumyr@nav.no>

---
+++ knot004/src/main/java/no/nav/doknotifikasjon/Validator.java
@@ -0,0 +1,64 @@
+package no.nav.doknotifikasjon;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.doknotifikasjon.exception.functional.DoknotifikasjonValidationException;
+import no.nav.doknotifikasjon.kafka.KafkaTopics;
+import no.nav.doknotifikasjon.kodeverk.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.FEILET_FUNCTIONAL_EXCEPTION_DIGDIR_KRR_PROXY;
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.FEILET_FUNCTIONAL_EXCEPTION_SIKKERHETSNIVAA;
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.FEILET_SIKKERHETSNIVAA;
+import static no.nav.doknotifikasjon.kafka.DoknotifikasjonStatusMessage.FEILET_TECHNICAL_EXCEPTION_DATABASE;
+import static no.nav.doknotifikasjon.kodeverk.Status.FEILET;
+import static no.nav.doknotifikasjon.kodeverk.Status.INFO;
+
+@Component
+@Slf4j
+public class Validator {
+
+	private static final List<String> feilmeldingerSomIkkeSkalBliProsessert = List.of(
+			FEILET_FUNCTIONAL_EXCEPTION_SIKKERHETSNIVAA,
+			FEILET_SIKKERHETSNIVAA,
+			FEILET_FUNCTIONAL_EXCEPTION_DIGDIR_KRR_PROXY,
+			FEILET_TECHNICAL_EXCEPTION_DATABASE
+	);
+
+	public void validateInput(DoknotifikasjonStatusTo doknotifikasjonStatusTo) {
+		validateField(doknotifikasjonStatusTo.getBestillingsId(), "bestillingsId");
+		validateField(doknotifikasjonStatusTo.getBestillerId(), "bestillerId");
+		validateField(doknotifikasjonStatusTo.getMelding(), "melding");
+		validateStatus(doknotifikasjonStatusTo.getStatus());
+	}
+
+	private void validateField(String field, String fieldName) {
+		if (field == null || field.trim().isEmpty()) {
+			throw new DoknotifikasjonValidationException(String.format("Valideringsfeil i knot004: Hendelse på kafka-topic " +
+					"%s har tom verdi for %s.", KafkaTopics.KAFKA_TOPIC_DOK_NOTIFIKASJON_STATUS, fieldName));
+		}
+	}
+
+	private void validateStatus(Status status) {
+		if (status == null) {
+			throw new DoknotifikasjonValidationException(String.format("Valideringsfeil i knot004: Hendelse på kafka-topic " +
+					"%s har tom verdi for status.", KafkaTopics.KAFKA_TOPIC_DOK_NOTIFIKASJON_STATUS));
+		}
+	}
+
+	public boolean erStatusInfoEllerFeiletMedSpesiellFeilmelding(Status status, String melding) {
+		if (INFO.equals(status)) {
+			log.info("Knot004 Melding med status {} skal ikke oppdatere status. Avslutter behandlingen.", INFO);
+			return true;
+		}
+
+		if (FEILET.equals(status)) {
+			if (feilmeldingerSomIkkeSkalBliProsessert.contains(melding)) {
+				log.info("Melding med status FEILET og melding={} skal ikke oppdatere status i Knot004. Avslutter behandlingen.", melding);
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ knot004/src/test/java/no/nav/doknotifikasjon/StatusChangeTest.java
@@ -0,0 +1,76 @@
+package no.nav.doknotifikasjon;
+
+import no.nav.doknotifikasjon.kodeverk.Status;
+import no.nav.doknotifikasjon.model.Notifikasjon;
+import org.junit.jupiter.api.Test;
+
+import static no.nav.doknotifikasjon.kodeverk.Status.FEILET;
+import static no.nav.doknotifikasjon.kodeverk.Status.FERDIGSTILT;
+import static no.nav.doknotifikasjon.kodeverk.Status.OPPRETTET;
+import static no.nav.doknotifikasjon.kodeverk.Status.OVERSENDT;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StatusChangeTest {
+
+	Knot004Service knot004Service = new Knot004Service(null, null, null, null);
+
+	@Test
+	public void shouldUpdateStatusFromOpprettetToOversendt() {
+		Notifikasjon notifikasjon = new Notifikasjon();
+		notifikasjon.setStatus(OPPRETTET);
+		DoknotifikasjonStatusTo updateStatusTo = doknotifikasjonStatusBuilder(OVERSENDT);
+		assertTrue(knot004Service.statusIsNewerThanPreviousStatus(
+				updateStatusTo,
+				notifikasjon
+		));
+	}
+
+	@Test
+	public void shouldUpdateStatusOversendtToFerdigstilt() {
+		Notifikasjon notifikasjon = new Notifikasjon();
+		notifikasjon.setStatus(OVERSENDT);
+		DoknotifikasjonStatusTo updateStatusTo = doknotifikasjonStatusBuilder(FERDIGSTILT);
+		assertTrue(knot004Service.statusIsNewerThanPreviousStatus(
+				updateStatusTo,
+				notifikasjon
+		));
+	}
+
+	@Test
+	public void shouldUpdateStatusFerdigstiltToFeilet() {
+		Notifikasjon notifikasjon = new Notifikasjon();
+		notifikasjon.setStatus(FERDIGSTILT);
+		DoknotifikasjonStatusTo updateStatusTo = doknotifikasjonStatusBuilder(FEILET);
+		assertTrue(knot004Service.statusIsNewerThanPreviousStatus(
+				updateStatusTo,
+				notifikasjon
+		));
+	}
+
+	@Test
+	public void shouldNotUpdateStatusFerdigstiltToOversendt() {
+		Notifikasjon notifikasjon = new Notifikasjon();
+		notifikasjon.setStatus(FERDIGSTILT);
+		DoknotifikasjonStatusTo updateStatusTo = doknotifikasjonStatusBuilder(OVERSENDT);
+		assertFalse(knot004Service.statusIsNewerThanPreviousStatus(
+				updateStatusTo,
+				notifikasjon
+		));
+	}
+
+	@Test
+	public void shouldNotUpdateStatusFeiletToOversendt() {
+		Notifikasjon notifikasjon = new Notifikasjon();
+		notifikasjon.setStatus(FEILET);
+		DoknotifikasjonStatusTo updateStatusTo = doknotifikasjonStatusBuilder(OVERSENDT);
+		assertFalse(knot004Service.statusIsNewerThanPreviousStatus(
+				updateStatusTo,
+				notifikasjon
+		));
+	}
+
+	private DoknotifikasjonStatusTo doknotifikasjonStatusBuilder(Status status) {
+		return new DoknotifikasjonStatusTo(null, null, status, null, null);
+	}
+}
