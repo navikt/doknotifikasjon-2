879e0ce35b5a5ae3990eab7e5512e9a5c5552e42
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-03-30 11:27:11 +0200

    Fikset retryable av kall til Altinn3
    * Kalle metode fra egen proxy fungerer ikke med `@Retryable`
    * Fjernet `@Metrics` da dette ikke ble brukt

--- core/src/main/java/no/nav/doknotifikasjon/consumer/altinn/AltinnVarselConsumer.java
+++ core/src/main/java/no/nav/doknotifikasjon/consumer/altinn/AltinnVarselConsumer.java
@@ -1,6 +1,9 @@
 package no.nav.doknotifikasjon.consumer.altinn;
 
+import no.nav.doknotifikasjon.exception.technical.AltinnTechnicalException;
 import no.nav.doknotifikasjon.kodeverk.Kanal;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,9 +15,20 @@ public interface AltinnVarselConsumer {
 
 	Optional<UUID> sendVarsel(Kanal kanal, String bestillingsId, String kontaktInfo, String fnr, String tekst, String tittel);
 
+	@Retryable(
+			retryFor = AltinnTechnicalException.class,
+			maxAttemptsExpression = "${retry.attempts:10}",
+			backoff = @Backoff(delayExpression = "${retry.delay:1000}", multiplier = 2, maxDelay = 60_000L)
+	)
 	default Optional<UUID> sendEpostVarsel(String bestillingsId, String kontaktInfo, String fnr, String tekst, String tittel) {
 		return sendVarsel(EPOST, bestillingsId, kontaktInfo, fnr, tekst, tittel);
 	}
+
+	@Retryable(
+			retryFor = AltinnTechnicalException.class,
+			maxAttemptsExpression = "${retry.attempts:10}",
+			backoff = @Backoff(delayExpression = "${retry.delay:1000}", multiplier = 2, maxDelay = 60_000L)
+	)
 	default Optional<UUID> sendSmsVarsel(String bestillingsId, String kontaktInfo, String fnr, String tekst) {
 		return sendVarsel(SMS, bestillingsId, kontaktInfo, fnr, tekst, "");
 	}
