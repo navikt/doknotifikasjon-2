306267ddf6620fc2654330bcb8291d43656570a2
Anders Opskar Voldsund <anders.voldsund@nav.no>
2026-04-22 09:13:41 +0200

    Legg på recordStats på cachar for å unngå feilmelding ved oppstart

--- core/src/test/java/no/nav/doknotifikasjon/LocalTestCacheConfig.java
+++ core/src/test/java/no/nav/doknotifikasjon/LocalTestCacheConfig.java
@@ -7,27 +7,29 @@ import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static no.nav.doknotifikasjon.config.LokalCacheConfig.ALTINN_TOKEN_CACHE;
 import static no.nav.doknotifikasjon.config.LokalCacheConfig.AZURE_TOKEN_CACHE;
-
 
 public class LocalTestCacheConfig {
 
 	@Bean
 	CacheManager cacheManager() {
 		SimpleCacheManager manager = new SimpleCacheManager();
+
 		manager.setCaches(List.of(
 			new CaffeineCache(AZURE_TOKEN_CACHE, Caffeine.newBuilder()
-				.expireAfterWrite(0, TimeUnit.MINUTES)
+				.expireAfterWrite(0, MINUTES)
 				.maximumSize(0)
 				.build()),
 			new CaffeineCache(ALTINN_TOKEN_CACHE, Caffeine.newBuilder()
-				.expireAfterWrite(0, TimeUnit.MINUTES)
+				.expireAfterWrite(0, MINUTES)
 				.maximumSize(0)
 				.build())
 		));
+
 		return manager;
 	}
+
 }
